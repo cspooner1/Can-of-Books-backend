01217f41db29958d67e42a66e4aa4d22bdbe389b
Xamel7 <123972550+Xamel7@users.noreply.github.com>
2023-06-06 14:45:26 -0500

    Lab11

---
+++ books.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+let bookModel
+
+        const bookSchema = new mongoose.Schema({
+            title: String,
+            description: String,
+            status: String
+        });
+
+        bookModel = mongoose.model("Book", bookSchema)
+
+
+        const arrayOfBooks = [
+            {
+                title: 'Book 1',
+                author: 'Author 1',
+                genre: 'Genre 1'
+            },
+            {
+                title: 'Book 2',
+                author: 'Author 2',
+                genre: 'Genre 2'
+            },
+            {
+                title: 'Book 3',
+                author: 'Author 3',
+                genre: 'Genre 3'
+            },
+        ];
+
+        bookModel.insertMany(arrayOfBooks);
+
+
+
+
+
+module.exports = bookModel
+// module.exports = bookSchema
--- server.js
+++ server.js
@@ -3,14 +3,27 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bookModel = require("./books");
+const { default: mongoose, connect } = require('mongoose')
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING)
+    .then(() => { console.log('Connected Successful')})
+  
 
-app.get('/test', (request, response) => {
+app.get('/books', async (request, response) => {
+  let allBooks =await bookModel.find();
+  response.send(allBooks)
+})
 
+app.get('/test', async (request, response) => {
+  await mongoose.connect(process.env.DATABASE_CONNECTION_STRING)
+    .catch((err) => {
+      console.log(err.message)
+    })
   response.send('test request received')
 
 })
